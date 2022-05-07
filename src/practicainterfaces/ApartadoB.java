208967feaac3d536fa119a3fcdb909034bb012b4
Joaquin <jtp0002@alu.medac.es>
2022-05-07 19:32:48 +0200

    apartado b sin intentos

--- src/practicainterfaces/ApartadoB.java
+++ src/practicainterfaces/ApartadoB.java
@@ -8,7 +8,7 @@ import java.awt.Color;
 
 public class ApartadoB extends javax.swing.JFrame {
 
-    boolean[] luces = new boolean[]{false, false, false, false, false, false, false};
+    int cont = 0;
 
     /**
      * Creates new form ApartadoB
@@ -34,46 +34,56 @@ public class ApartadoB extends javax.swing.JFrame {
         Boton5 = new javax.swing.JToggleButton();
         Boton6 = new javax.swing.JToggleButton();
         Boton7 = new javax.swing.JToggleButton();
+        bReinicio = new javax.swing.JButton();
+        tIntentos = new javax.swing.JLabel();
+        cIntentos = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setSize(new java.awt.Dimension(1300, 380));
 
+        Boton1.setBackground(java.awt.Color.gray);
         Boton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 Boton1ActionPerformed(evt);
             }
         });
 
+        Boton2.setBackground(java.awt.Color.gray);
         Boton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 Boton2ActionPerformed(evt);
             }
         });
 
+        Boton3.setBackground(java.awt.Color.gray);
         Boton3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 Boton3ActionPerformed(evt);
             }
         });
 
+        Boton4.setBackground(java.awt.Color.gray);
         Boton4.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 Boton4ActionPerformed(evt);
             }
         });
 
+        Boton5.setBackground(java.awt.Color.gray);
         Boton5.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 Boton5ActionPerformed(evt);
             }
         });
 
+        Boton6.setBackground(java.awt.Color.gray);
         Boton6.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 Boton6ActionPerformed(evt);
             }
         });
 
+        Boton7.setBackground(java.awt.Color.gray);
         Boton7.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 Boton7ActionPerformed(evt);
@@ -116,13 +126,33 @@ public class ApartadoB extends javax.swing.JFrame {
                 .addContainerGap(56, Short.MAX_VALUE))
         );
 
+        bReinicio.setText("Reiniciar");
+        bReinicio.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bReinicioActionPerformed(evt);
+            }
+        });
+
+        tIntentos.setText("Intentos:");
+
+        cIntentos.setEditable(false);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(334, 334, 334)
+                        .addComponent(bReinicio)
+                        .addGap(36, 36, 36)
+                        .addComponent(tIntentos)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(cIntentos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(406, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -130,7 +160,12 @@ public class ApartadoB extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(123, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(bReinicio)
+                    .addComponent(tIntentos)
+                    .addComponent(cIntentos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(88, Short.MAX_VALUE))
         );
 
         pack();
@@ -139,78 +174,243 @@ public class ApartadoB extends javax.swing.JFrame {
     private void Boton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Boton1ActionPerformed
         if (Boton1.isSelected()) {
             Boton1.setBackground(Color.yellow);
-            luces[0] = true;
-            if (Boton2.isSelected()) {
+            if (!Boton2.isSelected()) {
+                Boton2.setBackground(Color.yellow);
+                Boton2.setSelected(true);
+            } else {
                 Boton2.setBackground(Color.gray);
+                Boton2.setSelected(false);
             }
         } else {
             Boton1.setBackground(Color.gray);
-            luces[0] = false;
+            if (Boton2.isSelected()) {
+                Boton2.setBackground(Color.gray);
+                Boton2.setSelected(false);
+            } else {
+                Boton2.setBackground(Color.yellow);
+                Boton2.setSelected(true);
+            }
         }
     }//GEN-LAST:event_Boton1ActionPerformed
 
     private void Boton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Boton2ActionPerformed
         if (Boton2.isSelected()) {
             Boton2.setBackground(Color.yellow);
-            luces[1] = true;
-            if (Boton1.isSelected()) {
+            if (!Boton1.isSelected()) {
+                Boton1.setBackground(Color.yellow);
+                Boton1.setSelected(true);
+            } else {
                 Boton1.setBackground(Color.gray);
+                Boton1.setSelected(false);
+            }
+            if (!Boton3.isSelected()) {
+                Boton3.setBackground(Color.yellow);
+                Boton3.setSelected(true);
+            } else {
+                Boton3.setBackground(Color.gray);
+                Boton3.setSelected(false);
             }
         } else {
             Boton2.setBackground(Color.gray);
-            luces[1] = false;
+            if (Boton1.isSelected()) {
+                Boton1.setBackground(Color.gray);
+                Boton1.setSelected(false);
+            } else {
+                Boton1.setBackground(Color.yellow);
+                Boton1.setSelected(true);
+            }
+            if (Boton3.isSelected()) {
+                Boton3.setBackground(Color.gray);
+                Boton3.setSelected(false);
+            } else {
+                Boton3.setBackground(Color.yellow);
+                Boton3.setSelected(true);
+            }
         }
     }//GEN-LAST:event_Boton2ActionPerformed
 
     private void Boton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Boton3ActionPerformed
         if (Boton3.isSelected()) {
             Boton3.setBackground(Color.yellow);
-            luces[2] = true;
+            if (!Boton2.isSelected()) {
+                Boton2.setBackground(Color.yellow);
+                Boton2.setSelected(true);
+            } else {
+                Boton2.setBackground(Color.gray);
+                Boton2.setSelected(false);
+            }
+            if (!Boton4.isSelected()) {
+                Boton4.setBackground(Color.yellow);
+                Boton4.setSelected(true);
+            } else {
+                Boton4.setBackground(Color.gray);
+                Boton4.setSelected(false);
+            }
         } else {
             Boton3.setBackground(Color.gray);
-            luces[2] = false;
+            if (Boton2.isSelected()) {
+                Boton2.setBackground(Color.gray);
+                Boton2.setSelected(false);
+            } else {
+                Boton2.setBackground(Color.yellow);
+                Boton2.setSelected(true);
+            }
+            if (Boton4.isSelected()) {
+                Boton4.setBackground(Color.gray);
+                Boton4.setSelected(false);
+            } else {
+                Boton4.setBackground(Color.yellow);
+                Boton4.setSelected(true);
+            }
         }
     }//GEN-LAST:event_Boton3ActionPerformed
 
     private void Boton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Boton4ActionPerformed
         if (Boton4.isSelected()) {
             Boton4.setBackground(Color.yellow);
-            luces[3] = true;
+            if (!Boton3.isSelected()) {
+                Boton3.setBackground(Color.yellow);
+                Boton3.setSelected(true);
+            } else {
+                Boton3.setBackground(Color.gray);
+                Boton3.setSelected(false);
+            }
+            if (!Boton5.isSelected()) {
+                Boton5.setBackground(Color.yellow);
+                Boton5.setSelected(true);
+            } else {
+                Boton5.setBackground(Color.gray);
+                Boton5.setSelected(false);
+            }
         } else {
             Boton4.setBackground(Color.gray);
-            luces[3] = false;
+            if (Boton3.isSelected()) {
+                Boton3.setBackground(Color.gray);
+                Boton3.setSelected(false);
+            } else {
+                Boton3.setBackground(Color.yellow);
+                Boton3.setSelected(true);
+            }
+            if (Boton5.isSelected()) {
+                Boton5.setBackground(Color.gray);
+                Boton5.setSelected(false);
+            } else {
+                Boton5.setBackground(Color.yellow);
+                Boton5.setSelected(true);
+            }
         }
     }//GEN-LAST:event_Boton4ActionPerformed
 
     private void Boton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Boton5ActionPerformed
         if (Boton5.isSelected()) {
             Boton5.setBackground(Color.yellow);
-            luces[4] = true;
+            if (!Boton4.isSelected()) {
+                Boton4.setBackground(Color.yellow);
+                Boton4.setSelected(true);
+            } else {
+                Boton4.setBackground(Color.gray);
+                Boton4.setSelected(false);
+            }
+            if (!Boton6.isSelected()) {
+                Boton6.setBackground(Color.yellow);
+                Boton6.setSelected(true);
+            } else {
+                Boton6.setBackground(Color.gray);
+                Boton6.setSelected(false);
+            }
         } else {
             Boton5.setBackground(Color.gray);
-            luces[4] = false;
+            if (Boton4.isSelected()) {
+                Boton4.setBackground(Color.gray);
+                Boton4.setSelected(false);
+            } else {
+                Boton4.setBackground(Color.yellow);
+                Boton4.setSelected(true);
+            }
+            if (Boton6.isSelected()) {
+                Boton6.setBackground(Color.gray);
+                Boton6.setSelected(false);
+            } else {
+                Boton6.setBackground(Color.yellow);
+                Boton6.setSelected(true);
+            }
         }
     }//GEN-LAST:event_Boton5ActionPerformed
 
     private void Boton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Boton6ActionPerformed
         if (Boton6.isSelected()) {
             Boton6.setBackground(Color.yellow);
-            luces[5] = true;
+            if (!Boton5.isSelected()) {
+                Boton5.setBackground(Color.yellow);
+                Boton5.setSelected(true);
+            } else {
+                Boton5.setBackground(Color.gray);
+                Boton5.setSelected(false);
+            }
+            if (!Boton7.isSelected()) {
+                Boton7.setBackground(Color.yellow);
+                Boton7.setSelected(true);
+            } else {
+                Boton7.setBackground(Color.gray);
+                Boton7.setSelected(false);
+            }
         } else {
             Boton6.setBackground(Color.gray);
-            luces[5] = false;
+            if (Boton5.isSelected()) {
+                Boton5.setBackground(Color.gray);
+                Boton5.setSelected(false);
+            } else {
+                Boton5.setBackground(Color.yellow);
+                Boton5.setSelected(true);
+            }
+            if (Boton7.isSelected()) {
+                Boton7.setBackground(Color.gray);
+                Boton7.setSelected(false);
+            } else {
+                Boton7.setBackground(Color.yellow);
+                Boton7.setSelected(true);
+            }
         }
     }//GEN-LAST:event_Boton6ActionPerformed
 
     private void Boton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Boton7ActionPerformed
         if (Boton7.isSelected()) {
             Boton7.setBackground(Color.yellow);
-            luces[6] = true;
+            if (!Boton6.isSelected()) {
+                Boton6.setBackground(Color.yellow);
+                Boton6.setSelected(true);
+            } else {
+                Boton6.setBackground(Color.gray);
+                Boton6.setSelected(false);
+            }
         } else {
             Boton7.setBackground(Color.gray);
-            luces[6] = false;
+            if (Boton6.isSelected()) {
+                Boton6.setBackground(Color.gray);
+                Boton6.setSelected(false);
+            } else {
+                Boton6.setBackground(Color.yellow);
+                Boton6.setSelected(true);
+            }
         }
     }//GEN-LAST:event_Boton7ActionPerformed
+
+    private void bReinicioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bReinicioActionPerformed
+        Boton1.setSelected(false);
+        Boton1.setBackground(Color.gray);
+        Boton2.setSelected(false);
+        Boton2.setBackground(Color.gray);
+        Boton3.setSelected(false);
+        Boton3.setBackground(Color.gray);
+        Boton4.setSelected(false);
+        Boton4.setBackground(Color.gray);
+        Boton5.setSelected(false);
+        Boton5.setBackground(Color.gray);
+        Boton6.setSelected(false);
+        Boton6.setBackground(Color.gray);
+        Boton7.setSelected(false);
+        Boton7.setBackground(Color.gray);
+    }//GEN-LAST:event_bReinicioActionPerformed
 
     /**
      * @param args the command line arguments
@@ -255,6 +455,9 @@ public class ApartadoB extends javax.swing.JFrame {
     private javax.swing.JToggleButton Boton5;
     private javax.swing.JToggleButton Boton6;
     private javax.swing.JToggleButton Boton7;
+    private javax.swing.JButton bReinicio;
+    private javax.swing.JTextField cIntentos;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel tIntentos;
     // End of variables declaration//GEN-END:variables
 }
